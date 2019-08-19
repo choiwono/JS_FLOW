68fb38a9ba228462685ec7008cbcbee964fa37c9
choiwono <treasure_b@naver.com>
2019-08-19 23:56:52 +0900

    # 검색결과 구현 2 실습완료

--- lecture-vue/1-vanilla/js/controllers/MainController.js
+++ lecture-vue/1-vanilla/js/controllers/MainController.js
@@ -1,4 +1,6 @@
 import FormView from '../views/FormView.js'
+import ResultView from '../views/ResultView.js'
+import SearchModel from '../models/SearchModel.js'
 
 const tag = '[MainController]'
 
@@ -6,14 +8,28 @@ export default {
   init() {
     FormView.setup(document.querySelector('form'))
       .on('@submit', e => this.onSubmit(e.detail.input))
-      .on('@reset', e => this.onReset())  
+      .on('@reset', e => this.onReset())
+      
+    ResultView.setup(document.querySelector('#search-result'))  
   },
-  
+
+  search(query){
+    console.log(tag, 'search()', query);
+    SearchModel.list(query).then(data => {
+      this.onSearchResult(data)
+    })
+  },
+
   onSubmit(input) {
     console.log(tag, 'onSubmit()', input)
+    this.search(input)
   },
 
   onReset(){
     console.log(tag, 'onReset()')
+  },
+
+  onSearchResult(data){
+    ResultView.render(data)
   }
 }
---
+++ lecture-vue/1-vanilla/js/views/ResultView.js
@@ -0,0 +1,31 @@
+import View from './View.js'
+
+const tag = '[ResultView]';
+
+const ResultView = Object.assign(View)
+
+ResultView.setup = function(el) {
+    this.init(el)
+}
+
+ResultView.render = function(data = []){
+    console.log(tag,'render()',data)
+    this.el.innerHTML = data.length ? this.getSearchResultHtml(data) : this.messages.NO_RESULT
+    this.show()
+}
+
+ResultView.getSearchResultHtml = function (data) {
+    return data.reduce((html,item) => {
+        html += this.getSearchItemHtml(item)
+        return html
+    },'<ul>')+'</ul>'
+}
+
+ResultView.getSearchItemHtml = function(item){
+    return `<li>
+       <img src="${item.image}">
+       <p>${item.name}</p> 
+    </li>`
+}
+
+export default ResultView
