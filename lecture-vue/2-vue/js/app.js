0922eebde8d8d0bd467d0d97c7e730f319ed2483
choiwono <treasure_b@naver.com>
2019-08-27 23:18:33 +0900

    190827 pure javascript -> vue.js 변경작업

--- lecture-vue/2-vue/js/app.js
+++ lecture-vue/2-vue/js/app.js
@@ -1,19 +1,54 @@
+import SearchModel from '../models/SearchModel.js'
+import KeywordModel from '../models/KeywordModel.js'
+
 new Vue({
     el : '#app',
     data : {
-        query : ''
+        query : '',
+        submitted: false,
+        tabs: ['추천 검색어', '최근 검색어'],
+        selectedTab: '',
+        searchResult: [],
+        keywords: []
+    },
+    created(){
+        this.selectedTab = this.tabs[0]
+        this.onKeyword();
     },
     methods : {
         onSubmit(e){
-
+            this.search()
         },
         onReset(e){
-            let el = document.querySelector('#search-model');
-            el.style.display = 'none';
+            this.resetForm()
+            this.submitted = false;
+            this.searchResult = []
         },
         onKeyup(e){
-            if(!this.query.length)
-                this.onReset();
+            if(!this.query.length) this.onReset();
+        },
+        resetForm(){
+            this.query = ''
+        },
+        search(){
+            SearchModel.list().then(data => {
+                this.submitted = true
+                this.searchResult = data
+            })
+        },
+        onClickTab(tab){
+            if(tab === '추천 검색어'){
+                this.selectedTab = this.tabs[0]
+                this.onKeyword();
+            } else {
+                this.selectedTab = tab;
+                this.keywords = [];
+            }
+        },
+        onKeyword(){
+            KeywordModel.list().then(data => {
+                this.keywords = data;
+            })
         }
     }
 })
