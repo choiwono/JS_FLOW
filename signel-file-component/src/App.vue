2453b2f6f92fa9d5b99978b3d9c8651b8b7052b5
choiwono <treasure_b@naver.com>
2019-09-01 15:39:30 +0900

    # single File component 연습

---
+++ signel-file-component/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <header>
+      <h2 class="container">검색</h2>
+    </header>
+    <div class="container">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import FormComponent from './components/FormComponent.vue'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    'search-form':FormComponent
+  }
+}
+</script>
